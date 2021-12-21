f8602dfc806f6b5f1412d5e64d094fdb7c63933d
PC <shahbazk302@gmail.com>
2021-12-21 19:15:34 +0530

    Initial commit 1.0.0

---
+++ app/src/main/java/com/shahbaz/prettyprintjson/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shahbaz.prettyprintjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shahbaz.prettyprint.PrettyPrint;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
